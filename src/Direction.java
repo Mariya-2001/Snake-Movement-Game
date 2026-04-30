7969ec8d1a1f339c63d66f087bb2fe31a7db84f7
Mariya <lamkia2001@gmail.com>
2026-04-30 11:25:23 +0400

    Add fromString() method

--- src/Direction.java
+++ src/Direction.java
@@ -1,3 +1,19 @@
 public enum Direction {
         LEFT,RIGHT,UP,DOWN;
+
+    public static Direction fromString(String direction){
+
+        switch (direction.toLowerCase()){
+            case "left":
+                return LEFT;
+            case "right":
+                return RIGHT;
+            case "up":
+                return UP;
+            case "down":
+                return DOWN;
+            default:
+                return null;
+        }
+    }
 }
