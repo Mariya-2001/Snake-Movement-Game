d197bf762e317faeab155ddbdc5d1dd721e78560
Mariya <lamkia2001@gmail.com>
2026-04-30 11:32:19 +0400

    Add Movement() Method

--- src/Position.java
+++ src/Position.java
@@ -14,4 +14,19 @@ public class Position {
     public int getRows() {
         return rows;
     }
+
+    public Position movement(Direction dir) {
+        switch (dir) {
+            case LEFT:
+                return new Position(rows, columns - 1);
+            case RIGHT:
+                return new Position(rows, columns + 1);
+            case UP:
+                return new Position(rows - 1, columns);
+            case DOWN:
+                return new Position(rows + 1, columns);
+            default:
+                return this;
+        }
+    }
 }
