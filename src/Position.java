da012a3e1d6666bb7c01cd6cede5893a22a58eb6
Mariya <lamkia2001@gmail.com>
2026-04-30 11:30:56 +0400

    Basic Class with Fields and Constructor

---
+++ src/Position.java
@@ -0,0 +1,17 @@
+public class Position {
+    public int columns;
+    public int rows;
+
+    public Position(int rows,int columns){
+        this.columns=columns;
+        this.rows=rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+}
