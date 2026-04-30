49efc145128c9d6d212ab841a4057e587c53ada2
Mariya <lamkia2001@gmail.com>
2026-04-30 11:51:17 +0400

    Add Initialization Methods

--- src/Snake.java
+++ src/Snake.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.List;
 
 public class Snake {
     public LinkedList<Position> snakeBody;
@@ -7,6 +8,26 @@ public class Snake {
     public Snake(){
         this.snakeBody=new LinkedList<>();
         this.currDirection =Direction.RIGHT;
+
+    }
+
+    public void initializing(List<Position> positions){
+        snakeBody.clear();
+        snakeBody.addAll(positions); //Head is at the end
+
+    }
+
+    public void initializeCenter(GameMap gameMap){
+        snakeBody.clear();
+        int rowCenter = gameMap.getRows()/2;
+        int colStart = (gameMap.getColumns()/2)-2;
+
+        for(int x=0;x<5;x++){
+            Position position =new Position(rowCenter,colStart+x);
+            snakeBody.add(position);
+            gameMap.placeSnake(position);
+        }
+        currDirection=Direction.RIGHT;
 
     }
 
